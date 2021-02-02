5210ab5b0fb99586c9e0c69a62eae7842c165201
madhukar_n <madhukar@ves.io>
2021-02-02 15:37:40 -0800

    Update terraform provider volterra version to 0.0.6

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = "0.0.5"
+      version = "0.0.6"
     }
     aws   = ">= 3.22.0"
     null  = ">= 3.0"
