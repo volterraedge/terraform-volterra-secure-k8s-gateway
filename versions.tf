c0c49844740bd3972276704a6b47afa94d958ec3
madhukar_n <madhukar@ves.io>
2021-02-25 11:57:34 -0800

    Fix comments in readme
    closes: https://github.com/volterraedge/terraform-volterra-secure-k8s-gateway/issues/17

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = "0.1.1"
+      version = "0.1.2"
     }
     aws   = ">= 3.22.0"
     null  = ">= 3.0"
