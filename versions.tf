b4800070c86bc608ae27fbaccef1ef32bb0deae6
madhukar_n <madhukar@ves.io>
2021-04-06 09:15:45 +0530

    Update terraform provider volterra version
    closes: #35

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = "0.2.1"
+      version = "0.3.0"
     }
     aws   = ">= 3.22.0"
     null  = ">= 3.0"
