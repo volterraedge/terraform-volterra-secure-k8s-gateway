53f29bea376f3e4d6c0fe138c32288637ca86a80
alexcohen <alexcohen@ves.io>
2022-02-17 12:21:05 -0500

    set at last module version with working kubeconfig file management

--- aws.tf
+++ aws.tf
@@ -127,6 +127,7 @@ resource "aws_security_group_rule" "eks-cluster-ingress-volterra-node" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
   cluster_name    = var.skg_name
   cluster_version = "1.18"
   subnets         = local.eks_subnets
